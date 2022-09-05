0ea4e57572edd1d0dae218f1a946a768f581456a
Ken Lenhart <omnipaid@gmail.com>
2022-09-05 11:54:33 -0700

    Add contestants/index_spec.rb with tests for user story 2, add contestants_controller.rb

---
+++ spec/features/contestants/index_spec.rb
@@ -0,0 +1,34 @@
+require 'rails_helper'
+
+RSpec.describe 'contestant index page' do
+  # User Story 2 of 3
+
+  # As a visitor,
+  # When I visit the contestants index page ("/contestants")
+  # I see a list of names of all the contestants
+  # And under each contestants name I see a list of the projects (names) that they've been on
+
+  # (e.g.   Kentaro Kameyama
+  #         Projects: Litfit, Rug Tuxedo
+
+  #         Jay McCarroll
+  #         Projects: LeatherFeather)
+
+  describe 'When I visit the contestants index page ("/contestants")' do
+    before :each do
+      @jay = Contestant.create(name: "Jay McCarroll", age: 40, hometown: "LA", years_of_experience: 13)
+      @gret = Contestant.create(name: "Gretchen Jones", age: 36, hometown: "NYC", years_of_experience: 12)
+      @kent = Contestant.create(name: "Kentaro Kameyama", age: 30, hometown: "Boston", years_of_experience: 8)
+      @erin = Contestant.create(name: "Erin Robertson", age: 44, hometown: "Denver", years_of_experience: 15)
+    end
+    it 'I see a list of names of all the contestants' do
+      visit "/contestants"
+
+      expect(page).to have_content("#{@jay.name}")
+      expect(page).to have_content("#{@gret.name}")
+      expect(page).to have_content("#{@kent.name}")
+      expect(page).to have_content("#{@erin.name}")
+    end
+
+  end
+end
